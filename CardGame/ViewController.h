d93515e572b3bbd2592d7fd664d020962170fef1
leoeatle <liuyitao811@hotmail.com>
2016-12-26 19:56:25 +0800

    Initial Commit

---
+++ CardGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CardGame
+//
+//  Created by LeoEatle on 16/12/26.
+//  Copyright © 2016年 LeoEatle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CardGame/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CardGame
+//
+//  Created by LeoEatle on 16/12/26.
+//  Copyright © 2016年 LeoEatle. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
